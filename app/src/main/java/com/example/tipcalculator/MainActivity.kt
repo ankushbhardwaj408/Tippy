9a13a35941983fcfe011c435ddf4b3b7a0bd8a5c
Ankush Bhardwaj <ankushbhardwaj408@gmail.com>
2022-09-05 19:38:42 +0530

    Updated

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.tipcalculator
 
+import android.animation.ArgbEvaluator
 import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -10,6 +11,7 @@ import android.view.KeyEvent
 import android.widget.EditText
 import android.widget.SeekBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 private lateinit var base:TextView
@@ -17,6 +19,7 @@ private lateinit var base:TextView
     private lateinit var tip:TextView
     private lateinit var total:TextView
     private lateinit var percent:TextView
+    private lateinit var textview:TextView
    private var initial=15
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,13 +29,16 @@ private lateinit var base:TextView
         percent=findViewById(R.id.tvTipPercent)
         tip=findViewById(R.id.tvTipAmount)
         total=findViewById(R.id.tvTotalAmount)
+        textview=findViewById(R.id.textView)
       percent.text="$initial%"
        seekbar.progress=initial
+       colorchange(initial)
         seekbar.setOnSeekBarChangeListener(object :SeekBar.OnSeekBarChangeListener{
     override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
 
         percent.text="$p1%"
         compute()
+       colorchange(p1)
     }
 
     override fun onStartTrackingTouch(p0: SeekBar?) {}
@@ -53,6 +59,24 @@ private lateinit var base:TextView
 
 
     }
+
+    private fun colorchange(p1:Int) {
+        val text=when(p1){
+            in 0..9->"Poor"
+            in 10..14->"Acceptable"
+            in 15..19->"Good"
+            in 20..24->"Great"
+            else->"Amazing"
+        }
+        textview.text=text
+val color=ArgbEvaluator().evaluate(
+    p1.toFloat()/seekbar.max,
+    ContextCompat.getColor(this,R.color.worst_tip),
+    ContextCompat.getColor(this,R.color.best_tip)
+) as Int
+textview.setTextColor(color)
+    }
+
     private fun compute()
     {
         if(base.text.isEmpty()) {
